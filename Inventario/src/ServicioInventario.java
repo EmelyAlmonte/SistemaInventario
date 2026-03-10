41ad70ae5c82367861225be5cdcc4b49a86db0dd
maxwelkn <saelmaxwel@gmail.com>
2026-03-09 22:32:17 -0400

    Servicio del inventario creado

--- Inventario/src/ServicioInventario.java
+++ Inventario/src/ServicioInventario.java
@@ -1,4 +1,32 @@
 public class ServicioInventario {
-    
-    //HOLA MAXUEL
+    private Inventario inventario;
+
+    public ServicioInventario(Inventario inventario) {
+        this.inventario = inventario;
+    }
+
+    public void registrarProducto(String codigo, String nombre, int stock) {
+        if (stock < 0) {
+            System.out.println("Error: el stock no puede ser negativo.");
+            return;
+        }
+
+        Producto producto = new Producto(codigo, nombre, stock);
+        inventario.agregarProducto(producto);
+    }
+
+    public void consultarProducto(String codigo) {
+        Producto producto = inventario.buscarProducto(codigo);
+
+        if (producto != null) {
+            System.out.println("\nProducto encontrado:");
+            System.out.println(producto);
+        } else {
+            System.out.println("Producto no encontrado.");
+        }
+    }
+
+    public void listarProductos() {
+        inventario.mostrarProductos();
+    }
 }
